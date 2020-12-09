034d1a66a455f825c5c228b1a28c6694da180f6a
Jaemin.Park <once29@naver.com>
2020-12-09 15:23:49 +0900

    Fix class type

--- app/src/main/java/com/mashup/kotlin_practice4/SampleAdapterItem.kt
+++ app/src/main/java/com/mashup/kotlin_practice4/SampleAdapterItem.kt
@@ -8,6 +8,6 @@ sealed class SampleAdapterItem(open val id: Long) {
     override fun equals(other: Any?): Boolean = false
     override fun hashCode(): Int = id.hashCode()
 
-    class Text(override val id: Long, val text: String) : SampleAdapterItem(id)
-    class Image(override val id: Long, val imageUrl: String) : SampleAdapterItem(id)
+    data class Text(override val id: Long, val text: String) : SampleAdapterItem(id)
+    data class Image(override val id: Long, val imageUrl: String) : SampleAdapterItem(id)
 }

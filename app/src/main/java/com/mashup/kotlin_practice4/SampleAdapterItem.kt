1ff73f2c675cc3422b4f3ecfc269170ac9f69902
Jaemin.Park <once29@naver.com>
2020-12-09 15:14:19 +0900

    Implement SampleAdapterItem

--- app/src/main/java/com/mashup/kotlin_practice4/SampleAdapterItem.kt
+++ app/src/main/java/com/mashup/kotlin_practice4/SampleAdapterItem.kt
@@ -1,7 +1,13 @@
 package com.mashup.kotlin_practice4
 
-/** Practice4 
+/** Practice4
  * 1. sealed class SampleAdapterItem을 만들고 Text, Image 클래스를 생성
  * 2. SampleAdapterItem의 hashCode가 unique한 값을 갖게 설계하자
  **/
-object SampleAdapterItem
+sealed class SampleAdapterItem(open val id: Long) {
+    override fun equals(other: Any?): Boolean = false
+    override fun hashCode(): Int = id.hashCode()
+
+    class Text(override val id: Long, val text: String) : SampleAdapterItem(id)
+    class Image(override val id: Long, val imageUrl: String) : SampleAdapterItem(id)
+}

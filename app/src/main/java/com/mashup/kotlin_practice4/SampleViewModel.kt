f6e44cc301fafbe517ca874807c7628c3d4a8343
Jaemin.Park <once29@naver.com>
2020-12-09 15:16:13 +0900

    Add sample items to SampleViewModel

--- app/src/main/java/com/mashup/kotlin_practice4/SampleViewModel.kt
+++ app/src/main/java/com/mashup/kotlin_practice4/SampleViewModel.kt
@@ -9,7 +9,16 @@ class SampleViewModel @ViewModelInject constructor(
 ) : BaseViewModel(schedulerProvider) {
 
     companion object {
-        private val SAMPLE_ITEMS = listOf(SampleAdapterItem, SampleAdapterItem, SampleAdapterItem)
+        private val SAMPLE_ITEMS =
+            listOf(
+                SampleAdapterItem.Text(0, "hello"),
+                SampleAdapterItem.Text(1, "My name is"),
+                SampleAdapterItem.Text(2, "Jaemin"),
+                SampleAdapterItem.Image(
+                    3,
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScgioXP0bhnueOQVYBNz2C3lij0JdlffnOyw&usqp=CAU"
+                )
+            )
     }
 
     val items: Flowable<List<SampleAdapterItem>> = Flowable.just(SAMPLE_ITEMS).subscribeOnIO()
